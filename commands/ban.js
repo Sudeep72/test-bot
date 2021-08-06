5322370d4552661b58d171fb7d987d34f24efafe
Sudeep72 <79525645+Sudeep72@users.noreply.github.com>
2021-08-06 19:56:29 +0530

    new commit

---
+++ commands/ban.js
@@ -0,0 +1,15 @@
+module.exports = {
+    minArgs: 1,
+    requiredPermissions: ["ADMINISTRATOR", "BAN_MEMBERS"],
+    syntaxError: "Incorrect syntax, please use !ban <member>",
+    callback: ({ message }) => {
+      const { member, mentions } = message;
+      const target = mentions.users.first();
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id);
+        targetMember.ban();
+        message.channel.send(`<@${member.id}> has been banned`);
+      }
+    },
+  };
+  
